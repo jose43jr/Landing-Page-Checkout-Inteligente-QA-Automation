2135288a7e4a605fa48b8e7891fbec9900f92ce6
Jose Feitosa Junior <feitosa34jr@gmail.com>
2026-01-16 09:11:18 -0300

    fix: adicionando arquivo de configuração do Cypress

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    supportFile: false, // Desativamos o arquivo de suporte para simplificar este projeto
+  },
+});
